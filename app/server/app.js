0cc08e463ca7c044bd32af055c213284bdecefcf
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-03-11 22:28:11 +0300

    Installed request and cheerio. Example of usage

--- app/server/app.js
+++ app/server/app.js
@@ -4,6 +4,21 @@ var app = express();
 var routes = require('./routes');
 var config = require('./config');
 
+var request = require("request"),
+    cheerio = require("cheerio"),
+    url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=02888";
+
+request(url, function (error, response, body) {
+    if (!error) {
+        var $ = cheerio.load(body),
+            temperature = $("[data-variable='temperature'] .wx-value").html();
+
+        console.log("Температура " + temperature + " градусов по Фаренгейту.");
+    } else {
+        console.log("Произошла ошибка: " + error);
+    }
+});
+
 app.use(config.API_URL, routes);
 
 app.listen(config.PORT, function() {
