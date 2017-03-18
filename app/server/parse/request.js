3b2820391749fc105aa538a8291b65c7ec0c0e62
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-03-18 22:23:18 +0300

    Moved data fetching to API call (?)

---
+++ app/server/parse/request.js
@@ -0,0 +1,33 @@
+var request = require("request"),
+    cheerio = require("cheerio");
+
+var config = require('../config');
+
+request(config.SITE_URL, function (error, response, body) {
+    if (!error) {
+        var parsedResults = [];
+
+        var $ = cheerio.load(body);
+
+        $('div.item').each(function(i, element) {
+            var item = $(this);
+            var header = item.children('.itemhead').children('h3').children('a').prev().text();
+            var text = item.children('.itemtext').text();
+            var url = item.children('.itemhead').children('h3').children('a').prev().attr('href');
+            var img = item.children('.itemtext').children('p').children('a').children('img').attr("src");
+
+            var metadata = {
+                header: header,
+                text: text,
+                url: url,
+                img: img
+            };
+            parsedResults.push(metadata);
+        });
+        console.log(parsedResults);
+    } else {
+        console.log("Error: " + error);
+    }
+});
+
+module.exports = request;
--- app/server/routes/posts.js
+++ app/server/routes/posts.js
@@ -4,10 +4,47 @@ var router = express.Router();
 var db = require('../database/connection');
 var Post = require('../models/post');
 
+var request = require("request"),
+    cheerio = require("cheerio");
+
+var config = require('../config');
+
 // todo extract middleware functions and code refactor
 
 router.get('/', (req, res) => {
     res.send('Hello World from posts!')
+});
+
+router.get('/update', (req, res) => {
+
+    request(config.SITE_URL, function (error, response, body) {
+        if (!error) {
+            var parsedResults = [];
+
+            var $ = cheerio.load(body);
+
+            $('div.item').each(function(i, element) {
+                var item = $(this);
+                var header = item.children('.itemhead').children('h3').children('a').prev().text();
+                var text = item.children('.itemtext').text();
+                var url = item.children('.itemhead').children('h3').children('a').prev().attr('href');
+                var img = item.children('.itemtext').children('p').children('a').children('img').attr("src");
+
+                var metadata = {
+                    header: header,
+                    text: text,
+                    url: url,
+                    img: img
+                };
+                parsedResults.push(metadata);
+            });
+            console.log(parsedResults);
+            res.send('Posts are parsed!')
+        } else {
+            console.log("Error: " + error);
+            res.send("Error: " + error);
+        }
+    });
 });
 
 router.get('/:id', function(req, res) {
