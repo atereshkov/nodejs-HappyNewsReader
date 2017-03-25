e1a12e9cfef4d0a5d81f0eb5aab66a64f07514ad
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-03-26 01:32:18 +0300

    Implemented promise functionality for connection to database

--- app/server/app.js
+++ app/server/app.js
@@ -1,18 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
-var routes = require('./routes');
-var config = require('./config');
-// var path =require('path');
+const routes = require('./routes');
+const config = require('./config');
+const db = require('./database/connection');
+// var path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(config.API_URL, routes);
-// app.use(express.static(path.join(__dirname,config.STATIC_RESOURCES_PATH)));
+db()
+    .then(
+        onConnected => startServer(),
+        onError => console.log("Error due connection to db: " + onError.message)
+    );
 
-app.listen(config.PORT, function () {
-    console.log(`Working on ${config.PORT}`)
-});
+function startServer() {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(config.API_URL, routes);
+    // app.use(express.static(path.join(__dirname,config.STATIC_RESOURCES_PATH)));
+
+    app.listen(config.PORT, function () {
+        console.log(`Working on ${config.PORT}`)
+    });
+}
 
 module.exports = app;
