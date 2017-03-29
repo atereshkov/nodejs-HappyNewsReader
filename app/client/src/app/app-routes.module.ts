4169a7e22f4cb99e09aa9048838fe2f963b1c446
olegbal <olegbalah@gmail>
2017-03-29 21:30:00 +0200

    add /post route

--- app/client/src/app/app-routes.module.ts
+++ app/client/src/app/app-routes.module.ts
@@ -2,7 +2,7 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PostsComponent} from './components/postlist.component';
 const routes: Routes = [
-  // { path: '', redirectTo: '/posts', pathMatch: 'full' },
+   { path: '', redirectTo: '/posts', pathMatch: 'full' },
   { path: 'posts',  component: PostsComponent },
 ];
 @NgModule({
