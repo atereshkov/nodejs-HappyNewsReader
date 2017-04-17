bff1962ce1c1408cba9ae3bd0bc0093089c2c1c9
olegbal <olegbalah@gmail>
2017-04-17 21:46:40 +0200

    Add ngOninit method to root component

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title="Happy News Reader for S13.ru. Enjoy your news!";
+export class AppComponent implements OnInit {
+  title = "Happy News Reader for S13.ru. Enjoy your news!";
+
+  ngOnInit() {
+    localStorage.setItem("page", "1");
+    localStorage.setItem("limit", "10");
+  }
 }
--- app/client/src/app/services/post.service.ts
+++ app/client/src/app/services/post.service.ts
@@ -12,15 +12,17 @@ export class PostService {
      // Resolve HTTP using the constructor
      constructor (private http: Http) {}
      // private instance variable to hold base url
-     private commentsUrl = '/api/v1/posts';
+     private postsUrl = '/api/v1/posts';
 
      getAllPosts() : Observable<PostsWrapper> {
 
-             // ...using get request
-             return this.http.get(this.commentsUrl)
+             // ...using get
+             return this.http.get(this.postsUrl)
                             // ...and calling .json() on the response to return data
                              .map((res:Response) => res.json())
                              //...errors if any
                              .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
          }
+
+
 }
