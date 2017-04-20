9b314086d05f5a7d4b961d3f15e14527dcb67363
olegbal <olegbalah@gmail>
2017-04-20 23:31:41 +0200

    #24 some refactoring

--- app/client/src/app/components/postlist.component.ts
+++ app/client/src/app/components/postlist.component.ts
@@ -33,49 +33,58 @@ export class PostsComponent implements OnInit {
 
 
   getParsedPosts() {
+    this.getOnlinePosts();
+  }
 
-    this.postService.getPosts(this.page.toString(), this.limit.toString()).subscribe(
-      (recievedPosts) => {
+  getOnlinePosts() {
+    this.postService.getPosts(this.page.toString(), this.limit.toString())
+      .subscribe(
+        (recievedPosts) => {
 
-        if (recievedPosts.data.length > 0) {
+          if (recievedPosts.data.length > 0) {
 
-          this.unParsedPosts = recievedPosts.data;
-          this.removeLinks(this.unParsedPosts);
+            this.unParsedPosts = recievedPosts.data;
+            this.removeLinks(this.unParsedPosts);
 
-          for (var i = 0; i < this.unParsedPosts.length; i++) {
-            if (!this.posts.includes(this.unParsedPosts[i])) {
-              this.posts.push(this.unParsedPosts[i]);
-            }
-            this.indexedDbService.addPost(this.unParsedPosts[i]);
-
-          }
-          this.page++;
-
-          this.isInCompleted = false;
-        }
-      },
-      (error) => {
-        if (error.status == 0 || error.status == 500) {
-
-          this.indexedDbService.getPosts(this.page, this.limit).then((value) => {
-              this.unParsedPosts = value;
-              console.log(this.unParsedPosts);
-            }
-          ).then(() => {
-            if (this.posts.length == 0 && this.unParsedPosts.length != 0) {
-              this.posts.push(this.unParsedPosts[0]);
-            }
-            for (var j = 0; j < this.unParsedPosts.length; j++) {
-              if (!this.posts.includes(this.unParsedPosts[j])) {
-                this.posts.push(this.unParsedPosts[j]);
+            for (var i = 0; i < this.unParsedPosts.length; i++) {
+              if (!this.posts.includes(this.unParsedPosts[i])) {
+                this.posts.push(this.unParsedPosts[i]);
               }
+              this.indexedDbService.addPost(this.unParsedPosts[i]);
+
             }
-          });
-          this.isInCompleted = false;
-          this.page++;
+            this.page++;
+
+            this.isInCompleted = false;
+          }
+        },
+        (error) => {
+          if (error.status == 0 || error.status == 500) {
+            this.getOfflinePosts();
+          }
         }
-      }
-    );
+      );
+  }
+
+  getOfflinePosts() {
+    this.indexedDbService.getPosts(this.page, this.limit)
+      .then((value) => {
+          this.unParsedPosts = value;
+          console.log(this.unParsedPosts);
+        }
+      )
+      .then(() => {
+        if (this.posts.length == 0 && this.unParsedPosts.length != 0) {
+          this.posts.push(this.unParsedPosts[0]);
+        }
+        for (var j = 0; j < this.unParsedPosts.length; j++) {
+          if (!this.posts.includes(this.unParsedPosts[j])) {
+            this.posts.push(this.unParsedPosts[j]);
+          }
+        }
+      });
+    this.isInCompleted = false;
+    this.page++;
   }
 
   removeLinks(unparsedPosts: SummaryPost[]) {
